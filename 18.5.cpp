7fda0a020629236a61f975255e608864b47060c4
Pavel-Men <h702oy54rus@mail.ru>
2025-09-20 21:39:36 +0300

    Добавьте файлы проекта.

---
+++ 18.5.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Player
+{
+    int points = 0;
+    string name = "none";
+public:
+    Player() {}
+    Player(int _points, string _name) : points(_points), name(_name) {}
+
+    string getName() { return name; }
+    int getPoints() { return points; }
+
+    void setName(string& _name) { name = _name; }
+    void setPoints(int& _points) { points = _points; }
+};
+
+int main()
+{
+    cout << "Enter the number of players: ";
+    int count;
+    cin >> count;
+
+    Player* players = new Player[count];
+
+    string name;
+    int points;
+    for (int i = 0; i < count; i++)
+    {
+        cout << "Enter the name of player #" << i + 1 << ": ";
+        cin >> name;
+        players[i].setName(name);
+        cout << "Enter points of player #" << i + 1 << ": ";
+        cin >> points;
+        players[i].setPoints(points);
+    }
+    cout << endl;
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = i + 1; j < count; j++)
+        {
+            if (players[i].getPoints() < players[j].getPoints())
+            {
+                swap(players[i], players[j]);
+            }
+        }
+        cout << players[i].getName() << "\t" << players[i].getPoints() << endl;
+    }
+}
